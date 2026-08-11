7b4f20802956718832e22f61d0ce78f604982606
Ziesche Til Newman <tilnewman@protonmail.com>
2026-08-11 08:30:45 -0600

    started fly animation

--- src/enemy/fly.cpp
+++ src/enemy/fly.cpp
@@ -20,11 +20,14 @@ namespace shadowman
         const FlyTextures & t_textures,
         const sf::FloatRect & t_rect)
         : m_type{ t_type }
+        , m_isDying{ false }
         , m_rect{ t_rect }
         , m_sprite{ t_textures.fly.at(0) }
         , m_isFacingRight{ t_context.random.boolean() }
         , m_textures{ t_textures }
     {
+        util::setOriginToCenter(m_sprite);
+
         const float scale{ t_context.layout.scaleBasedOnResolution(
             t_context, t_context.setting.fly_scale) };
 
@@ -35,16 +38,33 @@ namespace shadowman
             m_sprite.scale({ -1.0f, 1.0f });
         }
 
-        m_sprite.setPosition(
-            { util::center(m_rect).x,
-              (util::center(m_rect).y - (m_sprite.getGlobalBounds().size.y * 0.5f)) });
+        m_sprite.setPosition(util::center(m_rect));
     }
 
-    void Fly::update(const Context &, const float) {}
-
-    void Fly::draw(const Context & t_context, sf::RenderTarget & t_target, sf::RenderStates t_states) const
+    void Fly::update(const Context &, const float t_elapsedSec)
     {
-        // TODO skip draw if no visible
+        m_animElapsedSec += t_elapsedSec;
+        const float timeBetweenFramees{ (m_isDying) ? 0.125f : 0.055f };
+        if (m_animElapsedSec > timeBetweenFramees)
+        {
+            m_animElapsedSec -= timeBetweenFramees;
+
+            const std::vector<sf::Texture> & textures{ (m_isDying) ? m_textures.die
+                                                                   : m_textures.fly };
+
+            if (++m_frameIndex >= textures.size())
+            {
+                m_frameIndex = 0;
+            }
+
+            m_sprite.setTexture(textures.at(m_frameIndex));
+        }
+    }
+
+    void Fly::draw(
+        const Context & t_context, sf::RenderTarget & t_target, sf::RenderStates t_states) const
+    {
+        // TODO skip draw if not visible
 
         sf::Sprite tempSprite{ m_sprite };
         tempSprite.move(t_context.level.mapToOffscreenOffset());
--- src/enemy/fly.hpp
+++ src/enemy/fly.hpp
@@ -57,6 +57,8 @@ namespace shadowman
             const FlyTextures & t_textures,
             const sf::FloatRect & t_rect);
 
+        [[nodiscard]] constexpr FlyType which() const noexcept { return m_type; }
+
         void update(const Context & t_context, const float t_elapsedSec);
 
         void draw(const Context & t_context, sf::RenderTarget & t_target, sf::RenderStates t_states)
@@ -64,9 +66,12 @@ namespace shadowman
 
       private:
         FlyType m_type;
+        bool m_isDying;
         sf::FloatRect m_rect;
         sf::Sprite m_sprite;
         bool m_isFacingRight;
+        float m_animElapsedSec;
+        std::size_t m_frameIndex;
         const FlyTextures & m_textures;
     };
 
